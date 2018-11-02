da655686584702a07741fe90ebe482fb1dcb9825
Julian Gonggrijp <dev@juliangonggrijp.com>
2018-11-02 01:39:11 +0100

    Simplify frontend testing with i18n

--- {{cookiecutter.slug}}/frontend/src/test-util.ts
+++ {{cookiecutter.slug}}/frontend/src/test-util.ts
@@ -1,5 +1,11 @@
 import '@dhl-uu/jquery-promise';
 
 import './global/hbsHelpers';
+import { i18nPromise } from './global/i18n';
 
-export { i18nPromise } from './global/i18n';
+/**
+ * Wrap this in a beforeAll() to enable i18n in all tests of a suite.
+ */
+export function enableI18n() {
+    return i18nPromise;
+}
