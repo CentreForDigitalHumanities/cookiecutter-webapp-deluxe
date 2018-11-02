e6f9b625265d16958ef8025c6f05c8216beea596
Julian Gonggrijp <dev@juliangonggrijp.com>
2018-11-02 01:40:16 +0100

    Use nicer config imports in the frontend

--- {{cookiecutter.slug}}/frontend/src/main.ts
+++ {{cookiecutter.slug}}/frontend/src/main.ts
@@ -2,7 +2,7 @@ import { history } from 'backbone';
 import { when, ready } from 'jquery';
 import '@dhl-uu/jquery-promise';
 
-import * as config from 'config.json';
+import { baseUrl } from 'config.json';
 import { i18nPromise } from './global/i18n';
 import './global/internalLinks';
 import './global/hbsHelpers';
@@ -10,7 +10,7 @@ import './aspects/example';
 
 when(ready, i18nPromise).done(function() {
     let success = history.start({
-        root: config.baseUrl,
+        root: baseUrl,
         pushState: true,
     });
 });
