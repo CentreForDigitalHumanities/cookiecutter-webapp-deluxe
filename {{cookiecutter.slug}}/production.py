f84375ebf36c09fef87a51f8eda666963becdae7
Sheean Spoel <s.j.j.spoel@uu.nl>
2019-04-29 11:04:55 +0200

    Run in debug mode in Travis to get error info

--- {{cookiecutter.slug}}/production.py
+++ {{cookiecutter.slug}}/production.py
@@ -1,3 +1,4 @@
+import os
 from collect import *
 
 STATIC_ROOT = None
@@ -5,3 +6,5 @@ STATIC_ROOT = None
 {% if cookiecutter.frontend == "angular" %}
 PROXY_FRONTEND = None # use statically compiled files
 {% endif %}
+if 'TRAVIS' in os.environ:
+    DEBUG = True
