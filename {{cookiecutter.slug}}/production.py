0c2604003d41bb1ddee95825ddf584f446d601a2
Sheean Spoel <s.j.j.spoel@uu.nl>
2019-04-29 11:37:50 +0200

    Actually use collected static files

--- {{cookiecutter.slug}}/production.py
+++ {{cookiecutter.slug}}/production.py
@@ -1,7 +1,11 @@
 import os
+import os.path as op
 from collect import *
 
-STATIC_ROOT = None
+here = op.dirname(op.abspath(__file__))
+
+STATICFILES_DIRS = [op.join(here, 'static')]
+STATIC_ROOT = '/'
 
 {% if cookiecutter.frontend == "angular" %}
 PROXY_FRONTEND = None # use statically compiled files
