440d8794d855801aac534f7ae250ec96d26b54e8
Alex Hebing <a.h.j.hebing@uu.nl>
2019-11-19 09:17:18 +0100

    Add a comment to the example view (#11)

--- {{cookiecutter.slug}}/backend/example/views.py
+++ {{cookiecutter.slug}}/backend/example/views.py
@@ -1,6 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+# This a very basic View that servers as an example.
+# Note that, when utilizing models and a database, etc, you would not have
+# views like this, but rather Viewsets. See the Django Rest Framework (DRF) documentation.
+
 @api_view()
 def hooray(request):
     response = [{ 'message': 'https://media.giphy.com/media/yoJC2GnSClbPOkV0eA/source.gif' }]
