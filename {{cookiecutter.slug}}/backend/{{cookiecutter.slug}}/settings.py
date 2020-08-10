e3a08df16259c9c4329fdf5040206a8a23eb1d44
BeritJanssen <berit.janssen@gmail.com>
2020-08-10 16:18:34 +0200

    working solution using collectstatic

--- {{cookiecutter.slug}}/backend/{{cookiecutter.slug}}/settings.py
+++ {{cookiecutter.slug}}/backend/{{cookiecutter.slug}}/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = []
 PROXY_FRONTEND = None
