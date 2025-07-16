0bf55499f7b8ac10eec38178c482405279321e0a
BeritJanssen <berit.janssen@gmail.com>
2025-07-16 15:29:30 +0200

    fix: conditional setting for development-idp so it can also be run locally

--- {{cookiecutter.slug}}/backend/{{cookiecutter.slug}}/saml_settings.py
+++ {{cookiecutter.slug}}/backend/{{cookiecutter.slug}}/saml_settings.py
@@ -1,18 +1,21 @@
 """ SAML settings for testing purposes """
 
-from django.urls import reverse_lazy
 from os.path import dirname, join
+from os import environ
+
 from cdh.federated_auth.saml.settings import create_saml_config
+from django.urls import reverse_lazy
 
 USER_AUTH_DIR = join(dirname(dirname(__file__)), 'user')
 APP_NAME = '{{cookiecutter.slug}}'
+IDP_ADDRESS = environ.get('IDP_ADDRESS', 'localhost')
 
 SAML_CONFIG = create_saml_config(
     base_url='localhost:8000',
     name=APP_NAME,
     key_file=join(USER_AUTH_DIR, 'tests', 'saml', 'sp_certificates', 'private.key'),
     cert_file=join(USER_AUTH_DIR, 'tests', 'saml', 'sp_certificates', 'public.cert'),
-    idp_metadata='http://test-idp:7000/saml/idp/metadata/',
+    idp_metadata=f'http://{IDP_ADDRESS}:7000/saml/idp/metadata/',
     contact_given_name='Research Software Lab, Utrecht University Centre for Digital Humanities',
     contact_email='digitalhumanities@uu.nl',
 )
