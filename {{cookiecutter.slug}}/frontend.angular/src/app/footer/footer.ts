9a7f732f8de1f190827fd7ab6ad84e89294b9be4
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-01-02 17:22:35 +0100

    Minor Footer refactor

--- {{cookiecutter.slug}}/frontend.angular/src/app/footer/footer.ts
+++ {{cookiecutter.slug}}/frontend.angular/src/app/footer/footer.ts
@@ -1,24 +1,28 @@
-import { Component, inject, LOCALE_ID, OnDestroy } from '@angular/core';
-import { formatDate } from '@angular/common';
+import { Component, computed, inject, LOCALE_ID, signal } from '@angular/core';
+import { CommonModule, formatDate } from '@angular/common';
 import { environment } from '../../environments/environment';
 import { DarkModeStore } from '../services/dark-mode-store';
+import { map } from 'rxjs';
 
 @Component({
-    selector: '{{cookiecutter.app_prefix}}-footer',
+    selector: 'dh-footer',
     templateUrl: './footer.html',
     styleUrl: './footer.scss',
+    imports: [CommonModule]
 })
-export class Footer implements OnDestroy {
+export class Footer {
     private darkModeStore = inject(DarkModeStore);
     private localeId = inject(LOCALE_ID);
 
-    public buildTime = formatDate(
+    public environment = signal(environment);
+
+    public buildTime = computed(() => formatDate(
         new Date(environment.buildTime),
         $localize`:@@dateFormat:MMMM dd, yyyy`,
         this.localeId
-    );
+    ));
 
-    public dark$ = this.darkModeStore.theme$.pipe(
-        map(theme => theme === 'dark')
+    public logoSrc = this.darkModeStore.theme$.pipe(
+        map(theme => `${environment.assets}${theme === 'dark' ? '/uu-cdh-dark.svg' : '/uu-cdh.svg'}`)
     );
 }
