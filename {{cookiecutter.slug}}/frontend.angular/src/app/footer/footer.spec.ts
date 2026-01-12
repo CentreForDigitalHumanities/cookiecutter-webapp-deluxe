18cfad6581935db2ae49d344e1ddf3cc700f9836
Xander Vertegaal <a.j.j.vertegaal@uu.nl>
2026-01-12 14:13:01 +0100

    Merge double beforeEach blocks

--- {{cookiecutter.slug}}/frontend.angular/src/app/footer/footer.spec.ts
+++ {{cookiecutter.slug}}/frontend.angular/src/app/footer/footer.spec.ts
@@ -6,11 +6,8 @@ describe('Footer', () => {
     let component: Footer;
     let fixture: ComponentFixture<Footer>;
 
-    beforeEach((() => {
-        TestBed.configureTestingModule({});
-    }));
-
     beforeEach(() => {
+        TestBed.configureTestingModule({});
         fixture = TestBed.createComponent(Footer);
         component = fixture.componentInstance;
         fixture.detectChanges();
